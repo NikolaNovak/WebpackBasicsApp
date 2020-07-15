dc14d98f5b2bdd85082d426326858bb503209bbc
Nikola Novak <nikola.novak.hr@gmail.com>
2020-07-16 00:13:32 +0200

    Setting up the basic webpack config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import classes from "./index.module.css";
+import "./index.css";
 import App from "./App";
 
 const app = (
